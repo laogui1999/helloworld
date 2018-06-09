e841a2d28968cc0c32563d47bd9efd2e29b39540
laogui1999 <1057082709@qq.com>
2018-06-09 14:55:07 +0800

    Update HelloWorld.java

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -3,7 +3,7 @@ package helloworld;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("hello world Ëæ±ã¸ÄÒ»¸Ä");
+		System.out.println("hello world ä½ å¥½");
 	}
 
 }
